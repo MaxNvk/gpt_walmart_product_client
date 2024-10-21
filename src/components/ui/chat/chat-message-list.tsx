59748e9267a79f48b1ee675fe152f503922be0e1
Max Novikov <max.vashuk@gmail.com>
2024-10-20 23:48:17 -0700

    init;

--- src/components/ui/chat/chat-message-list.tsx
+++ src/components/ui/chat/chat-message-list.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn.util";
 
 interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {}
 
